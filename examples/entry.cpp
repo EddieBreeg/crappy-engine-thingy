f9aa31b95736a5f044e348c94c99f9d5a5a02dda
EddieBreeg <eddiebreeg0@protonmail.com>
2023-11-07 17:21:37 -0500

    changed weird Sandbox app API

--- examples/entry.cpp
+++ examples/entry.cpp
@@ -36,18 +36,16 @@ private:
 
 public:
 	[[nodiscard]]
-	static Sandbox &Init(libstra::array_view<const char *> args) {
-		auto *app = new Sandbox(args); // cleaned up by the engine
-		return *app;
+	static std::unique_ptr<Sandbox>
+	Init(libstra::array_view<const char *> args) {
+		return std::unique_ptr<Sandbox>(new Sandbox(args));
 	}
-	void Terminate() { delete this; }
 	~Sandbox() { ET_LOG_INFO("Destroyed app"); }
 };
 
 int main(int argc, char const *argv[]) {
-	auto &app = Sandbox::Init({ argv, argv + argc });
-	app.Run();
+	auto app = Sandbox::Init({ argv, argv + argc });
+	app->Run();
 
-	app.Terminate();
 	return 0;
 }
